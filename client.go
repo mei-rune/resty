20db7ce009025a7a8880ff030d76caa308afc29c
meifakun <runner.mei@gmail.com>
2019-06-19 12:56:46 +0800

    init repo

---
+++ client.go
@@ -0,0 +1,468 @@
+package resty
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/runner-mei/errors"
+)
+
+const (
+	charsetUTF8 = "charset=UTF-8"
+	// PROPFIND Method can be used on collection and property resources.
+	PROPFIND = "PROPFIND"
+)
+
+// MIME types
+const (
+	MIMEApplicationJSON                  = "application/json"
+	MIMEApplicationJSONCharsetUTF8       = MIMEApplicationJSON + "; " + charsetUTF8
+	MIMEApplicationJavaScript            = "application/javascript"
+	MIMEApplicationJavaScriptCharsetUTF8 = MIMEApplicationJavaScript + "; " + charsetUTF8
+	MIMEApplicationXML                   = "application/xml"
+	MIMEApplicationXMLCharsetUTF8        = MIMEApplicationXML + "; " + charsetUTF8
+	MIMETextXML                          = "text/xml"
+	MIMETextXMLCharsetUTF8               = MIMETextXML + "; " + charsetUTF8
+	MIMEApplicationForm                  = "application/x-www-form-urlencoded"
+	MIMEApplicationProtobuf              = "application/protobuf"
+	MIMEApplicationMsgpack               = "application/msgpack"
+	MIMETextHTML                         = "text/html"
+	MIMETextHTMLCharsetUTF8              = MIMETextHTML + "; " + charsetUTF8
+	MIMETextPlain                        = "text/plain"
+	MIMETextPlainCharsetUTF8             = MIMETextPlain + "; " + charsetUTF8
+	MIMEMultipartForm                    = "multipart/form-data"
+	MIMEOctetStream                      = "application/octet-stream"
+
+	HeaderContentType = "Content-Type"
+)
+
+type HTTPError = errors.HTTPError
+
+var TimeFormat = time.RFC3339
+var ErrBadArgument = errors.ErrBadArgument
+var WithHTTPCode = errors.WithHTTPCode
+var Wrap = errors.Wrap
+
+var BufferPool sync.Pool
+
+func init() {
+	BufferPool.New = func() interface{} {
+		return bytes.NewBuffer(make([]byte, 0, 1024))
+	}
+}
+
+type ResponseFunc func(req *http.Request, resp *http.Response) error
+
+func New(urlStr string) (*Proxy, error) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+	var queryParams = url.Values{}
+	for key, values := range u.Query() {
+		queryParams[key] = values
+	}
+	u.RawQuery = ""
+
+	return &Proxy{
+		Client:      http.DefaultClient,
+		TimeFormat:  TimeFormat,
+		u:           *u,
+		queryParams: queryParams,
+		headers:     url.Values{},
+	}, nil
+}
+
+type Proxy struct {
+	Client        *http.Client
+	TimeFormat    string
+	jsonUseNumber bool
+	u             url.URL
+	queryParams   url.Values
+	headers       url.Values
+}
+
+func (px *Proxy) Clone() *Proxy {
+	queryParams := url.Values{}
+	for key, value := range px.queryParams {
+		queryParams[key] = value
+	}
+
+	headers := url.Values{}
+	for key, value := range px.headers {
+		headers[key] = value
+	}
+
+	return &Proxy{
+		Client:      px.Client,
+		TimeFormat:  px.TimeFormat,
+		u:           px.u,
+		queryParams: queryParams,
+		headers:     headers,
+	}
+}
+func (px *Proxy) JoinURL(urlStr string) *Proxy {
+	copyed := px.Clone()
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	for key, value := range u.Query() {
+		copyed.queryParams[key] = value
+	}
+	copyed.u.Path = Join(px.u.Path, u.Path)
+	return copyed
+}
+func (px *Proxy) JSONUseNumber() *Proxy {
+	px.jsonUseNumber = true
+	return px
+}
+func (px *Proxy) SetHeader(key, value string) *Proxy {
+	px.headers.Set(key, value)
+	return px
+}
+func (px *Proxy) AddHeader(key, value string) *Proxy {
+	px.headers.Add(key, value)
+	return px
+}
+func (px *Proxy) SetParam(key, value string) *Proxy {
+	px.queryParams.Set(key, value)
+	return px
+}
+func (px *Proxy) AddParam(key, value string) *Proxy {
+	px.queryParams.Add(key, value)
+	return px
+}
+func (px *Proxy) SetContentType(contentType string) *Proxy {
+	px.headers.Set(HeaderContentType, contentType)
+	return px
+}
+
+func (proxy *Proxy) Release(request *Request) {}
+
+func (proxy *Proxy) New(urlStr string) *Request {
+	r := &Request{
+		proxy:         proxy,
+		jsonUseNumber: proxy.jsonUseNumber,
+		u:             proxy.u,
+		queryParams:   url.Values{},
+		headers:       url.Values{},
+	}
+
+	for key, values := range proxy.queryParams {
+		r.queryParams[key] = values
+	}
+	for key, values := range proxy.headers {
+		r.headers[key] = values
+	}
+
+	if urlStr != "" {
+		u, err := url.Parse(urlStr)
+		if err != nil {
+			panic(err)
+		}
+
+		if u.Scheme != "" {
+			r.u = *u
+		} else {
+			r.u.Path = Join(r.u.Path, u.Path)
+		}
+
+		for key, values := range u.Query() {
+			r.queryParams[key] = values
+		}
+	}
+
+	return r
+}
+
+type Request struct {
+	proxy         *Proxy
+	jsonUseNumber bool
+	u             url.URL
+	queryParams   url.Values
+	headers       url.Values
+	requestBody   interface{}
+	exceptedCode  int
+	responseBody  interface{}
+}
+
+func (r *Request) JSONUseNumber() *Request {
+	r.jsonUseNumber = true
+	return r
+}
+func (r *Request) SetHeader(key, value string) *Request {
+	r.headers.Set(key, value)
+	return r
+}
+func (r *Request) AddHeader(key, value string) *Request {
+	r.headers.Add(key, value)
+	return r
+}
+func (r *Request) SetParam(key, value string) *Request {
+	r.queryParams.Set(key, value)
+	return r
+}
+func (r *Request) AddParam(key, value string) *Request {
+	r.queryParams.Add(key, value)
+	return r
+}
+func (r *Request) SetBody(body interface{}) *Request {
+	r.requestBody = body
+	return r
+}
+func (r *Request) Result(body interface{}) *Request {
+	r.responseBody = body
+	return r
+}
+func (r *Request) ExceptedCode(code int) *Request {
+	r.exceptedCode = code
+	return r
+}
+func (r *Request) GET(ctx context.Context) error {
+	return r.invoke(ctx, "GET")
+}
+func (r *Request) POST(ctx context.Context) error {
+	return r.invoke(ctx, "POST")
+}
+func (r *Request) PUT(ctx context.Context) error {
+	return r.invoke(ctx, "PUT")
+}
+func (r *Request) CONNECT(ctx context.Context) error {
+	return r.invoke(ctx, "CONNECT")
+}
+func (r *Request) DELETE(ctx context.Context) error {
+	return r.invoke(ctx, "DELETE")
+}
+func (r *Request) HEAD(ctx context.Context) error {
+	return r.invoke(ctx, "HEAD")
+}
+func (r *Request) OPTIONS(ctx context.Context) error {
+	return r.invoke(ctx, "OPTIONS")
+}
+func (r *Request) PATCH(ctx context.Context) error {
+	return r.invoke(ctx, "PATCH")
+}
+func (r *Request) TRACE(ctx context.Context) error {
+	return r.invoke(ctx, "TRACE")
+}
+func (r *Request) Do(ctx context.Context, method string) error {
+	return r.invoke(ctx, method)
+}
+func (r *Request) invoke(ctx context.Context, method string) error {
+	var req *http.Request
+
+	var body io.Reader
+	if r.requestBody != nil && method != "GET" {
+		switch value := r.requestBody.(type) {
+		case []byte:
+			body = bytes.NewReader(value)
+		case string:
+			body = strings.NewReader(value)
+		case io.Reader:
+			body = value
+		default:
+			buffer := BufferPool.Get().(*bytes.Buffer)
+			e := json.NewEncoder(buffer).Encode(r.requestBody)
+			if e != nil {
+				return WithHTTPCode(http.StatusBadRequest, e)
+			}
+			body = buffer
+			defer func() {
+				buffer.Reset()
+				BufferPool.Put(buffer)
+			}()
+		}
+	}
+
+	r.u.RawQuery = r.queryParams.Encode()
+	urlStr := r.u.String()
+	req, e := http.NewRequest(method, urlStr, body)
+	if e != nil {
+		return WithHTTPCode(http.StatusBadRequest, e)
+	}
+	if ctx != nil {
+		req = req.WithContext(ctx)
+	}
+	for key, values := range r.headers {
+		req.Header[key] = values
+	}
+
+	client := r.proxy.Client
+	if client == nil {
+		client = http.DefaultClient
+	}
+	resp, e := client.Do(req)
+	if e != nil {
+		return WithHTTPCode(http.StatusServiceUnavailable, e)
+	}
+
+	isOK := false
+	if r.exceptedCode == 0 {
+		if resp.StatusCode >= http.StatusOK && resp.StatusCode <= 299 {
+			isOK = true
+		}
+	} else if resp.StatusCode == r.exceptedCode {
+		isOK = true
+	}
+
+	if !isOK {
+		var responseBody string
+
+		if nil != resp.Body {
+			respBody, e := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+
+			if e != nil {
+				responseBody = string(respBody) + "\r\n*************** "
+				responseBody += e.Error()
+				responseBody += "***************"
+			} else {
+				responseBody = string(respBody)
+			}
+		}
+
+		if len(responseBody) == 0 {
+			return WithHTTPCode(resp.StatusCode, errors.New("request '"+urlStr+"' fail: "+resp.Status+": read_error"))
+		}
+		return WithHTTPCode(resp.StatusCode, errors.New("request '"+urlStr+"' fail: "+resp.Status+": "+responseBody))
+	}
+
+	// Install closing the request body (if any)
+	defer func() {
+		if nil != resp.Body {
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+	}()
+
+	if r.responseBody == nil {
+		return nil
+	}
+
+	switch response := r.responseBody.(type) {
+	case ResponseFunc:
+		return response(req, resp)
+	case *string:
+		var sb strings.Builder
+		if _, e = io.Copy(&sb, resp.Body); e != nil {
+			return WithHTTPCode(11, Wrap(e, "request '"+method+"' is ok and read response fail"))
+		}
+		*response = sb.String()
+		return nil
+	case *[]byte:
+		buffer := bytes.NewBuffer(make([]byte, 0, 1024))
+		if _, e = io.Copy(buffer, resp.Body); e != nil {
+			return WithHTTPCode(11, Wrap(e, "request '"+method+"' is ok and read response fail"))
+		}
+		*response = buffer.Bytes()
+		return nil
+	case io.Writer:
+		_, e = io.Copy(response, resp.Body)
+		if e == nil {
+			return nil
+		}
+		return WithHTTPCode(11, e)
+	default:
+		if r.jsonUseNumber {
+			decoder := json.NewDecoder(resp.Body)
+			decoder.UseNumber()
+			e = decoder.Decode(response)
+			if e != nil {
+				return WithHTTPCode(12, Wrap(e, "request '"+method+"' is ok and read response fail"))
+			}
+			return nil
+		}
+
+		buffer := BufferPool.Get().(*bytes.Buffer)
+		_, e = io.Copy(buffer, resp.Body)
+		if e != nil {
+			buffer.Reset()
+			BufferPool.Put(buffer)
+			return WithHTTPCode(11, Wrap(e, "request '"+method+"' is ok and read response fail"))
+		}
+
+		e = json.Unmarshal(buffer.Bytes(), response)
+		buffer.Reset()
+		BufferPool.Put(buffer)
+		if e != nil {
+			return WithHTTPCode(12, Wrap(e, "request '"+method+"' is ok and read response fail"))
+		}
+		return nil
+	}
+}
+
+// Join 拼接 url
+func Join(paths ...string) string {
+	switch len(paths) {
+	case 0:
+		return ""
+	case 1:
+		return paths[0]
+	case 2:
+		lastSplash := strings.HasSuffix(paths[0], "/")
+		if lastSplash {
+			if strings.HasPrefix(paths[1], "/") {
+				return paths[0] + paths[1][1:]
+			}
+			return paths[0] + paths[1]
+		} else if strings.HasPrefix(paths[1], "/") {
+			return paths[0] + paths[1]
+		}
+		return paths[0] + "/" + paths[1]
+	default:
+		return JoinWith(paths[0], paths[1:])
+	}
+}
+
+// JoinWith 拼接 url
+func JoinWith(base string, paths []string) string {
+	var buf strings.Builder
+	buf.WriteString(base)
+
+	lastSplash := strings.HasSuffix(base, "/")
+	for _, pa := range paths {
+		if 0 == len(pa) {
+			continue
+		}
+
+		if lastSplash {
+			if '/' == pa[0] {
+				buf.WriteString(pa[1:])
+			} else {
+				buf.WriteString(pa)
+			}
+		} else {
+			if '/' != pa[0] {
+				buf.WriteString("/")
+			}
+			buf.WriteString(pa)
+		}
+
+		lastSplash = strings.HasSuffix(pa, "/")
+	}
+	return buf.String()
+}
+
+// func NewRequest(proxy Proxy, url string) Request {
+//  return nil
+// }
+
+// func ReleaseRequest(proxy Proxy, request Request) {
+// }
+
+func NewRequest(proxy *Proxy, urlStr string) *Request {
+	return proxy.New(urlStr)
+}
+
+func ReleaseRequest(proxy *Proxy, r *Request) {
+	proxy.Release(r)
+}
---
+++ client_test.go
@@ -0,0 +1,476 @@
+package resty
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type TestData struct {
+	exceptedMethod  string
+	exceptedHeaders url.Values
+	exceptedURL     string
+	exceptedBody    string
+	responseCode    int
+	responseBody    string
+}
+
+func echoFunc(t *testing.T, data *TestData) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if data.exceptedMethod != "" {
+			if data.exceptedMethod != r.Method {
+				t.Error("method excepted:", data.exceptedMethod)
+				t.Error("method actual  :", r.Method)
+			}
+		}
+
+		if len(data.exceptedHeaders) > 0 {
+			for key, values := range data.exceptedHeaders {
+				actual := r.Header[key]
+				if !reflect.DeepEqual(actual, values) {
+					t.Error("header excepted:", key, values)
+					t.Error("header actual  :", key, actual)
+				}
+			}
+		}
+
+		if data.exceptedURL != "" {
+			if actual := r.URL.String(); actual != data.exceptedURL {
+				t.Error("url excepted:", data.exceptedURL)
+				t.Error("url actual  :", actual)
+			}
+		}
+
+		if data.exceptedBody != "" {
+			bs, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if s := string(bs); data.exceptedBody != s {
+				t.Error("body excepted:", data.exceptedBody)
+				t.Error("body actual  :", s)
+			}
+		}
+
+		if data.responseCode == 0 {
+			data.responseCode = http.StatusOK
+		}
+		if len(data.responseBody) == 0 {
+			data.responseBody = "OK"
+		}
+
+		w.WriteHeader(data.responseCode)
+		w.Write([]byte(data.responseBody))
+	})
+}
+
+func assetBody(t *testing.T, req *Request, code int, excepted string) *Request {
+	return req.ExceptedCode(code).
+		Result(ResponseFunc(func(r *http.Request, res *http.Response) error {
+			bs, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Error(err)
+				return nil
+			}
+			if s := string(bs); s != excepted {
+				t.Errorf("response excepted: %s", excepted)
+				t.Errorf("response actual  : %s", s)
+			}
+			return nil
+		}))
+}
+
+func TestGetFail(t *testing.T) {
+	failMessage := "FAIL message"
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "",
+		responseCode:    http.StatusInternalServerError,
+		responseBody:    failMessage,
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err == nil {
+		t.Error("err is nil")
+		return
+	}
+
+	if !strings.Contains(err.Error(), failMessage) {
+		t.Errorf("response excepted: %s", failMessage)
+		t.Errorf("response actual  : %s", err)
+	}
+}
+
+func TestGetOK(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPutOK(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "PUT",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "test",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		SetBody("test").
+		PUT(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPostOK(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "POST",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "test",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		SetBody("test").
+		POST(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDeleteOK(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "DELETE",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "test",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		SetBody("test").
+		DELETE(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSetHeader(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		AddHeader("Yaaa", "1").
+		AddHeader("Yaaa", "1").
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestAddHeader(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc", "1", "1"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		AddHeader("Yaaa", "1").
+		AddHeader("Yaaa", "1").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSetParam(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod: "GET",
+		exceptedURL:    "/test1/a?Yaaa=abc",
+		exceptedBody:   "",
+		responseCode:   http.StatusOK,
+		responseBody:   "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		AddParam("Yaaa", "1").
+		AddParam("Yaaa", "1").
+		SetParam("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestAddParam(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod: "GET",
+		exceptedURL:    "/test1/a?Yaaa=abc&Yaaa=1&Yaaa=1",
+		exceptedBody:   "",
+		responseCode:   http.StatusOK,
+		responseBody:   "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetParam("Yaaa", "abc").
+		AddParam("Yaaa", "1").
+		AddParam("Yaaa", "1").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBytesBody(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "POST",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    "test",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		SetBody([]byte("test")).
+		POST(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestJsonBody(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "POST",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		exceptedBody:    `{"a":"b"}` + "\n",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		SetBody(map[string]interface{}{"a": "b"}).
+		POST(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetBytes(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	var body []byte
+	err := prx.New("/a").
+		Result(&body).
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(body) != "OK" {
+		t.Errorf("body %s", body)
+	}
+}
+
+func TestGetString(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		responseCode:    http.StatusOK,
+		responseBody:    "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	var body string
+	err := prx.New("/a").
+		Result(&body).
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if body != "OK" {
+		t.Errorf("body %s", body)
+	}
+}
+
+func TestGetJson(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		responseCode:    http.StatusOK,
+		responseBody:    "{\"a\":\"b\"}",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	var body map[string]string
+	err := prx.New("/a").
+		Result(&body).
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if body["a"] != "b" {
+		t.Errorf("body %s", body)
+	}
+}
+
+func TestGetWriter(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod:  "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"}},
+		exceptedURL:     "/test1/a",
+		responseCode:    http.StatusOK,
+		responseBody:    "ok",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1")
+	prx, _ := New(urlStr)
+
+	var body bytes.Buffer
+	err := prx.New("/a").
+		Result(&body).
+		SetHeader("Yaaa", "abc").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if body.String() != "ok" {
+		t.Errorf("body %s", body.String())
+	}
+}
+
+func TestProxyData(t *testing.T) {
+	hsrv := httptest.NewServer(echoFunc(t, &TestData{
+		exceptedMethod: "GET",
+		exceptedHeaders: url.Values{"Yaaa": []string{"abc"},
+			"Ybbb": []string{"abb"}},
+		exceptedURL:  "/test1/a?a=b&c=d",
+		exceptedBody: "",
+		responseCode: http.StatusOK,
+		responseBody: "OK",
+	}))
+	defer hsrv.Close()
+
+	urlStr := Join(hsrv.URL, "/test1?c=d")
+	prx, _ := New(urlStr)
+	prx.SetHeader("Ybbb", "abb")
+
+	err := assetBody(t, prx.New("/a"), 0, "OK").
+		SetHeader("Yaaa", "abc").
+		SetParam("a", "b").
+		GET(nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
