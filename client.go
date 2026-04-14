d39bea370f6318fd3bdaa2428ee26b447b810701
mfk <mfk@hengwei.com.cn>
2026-04-14 19:22:15 +0800

    fix bugs

--- client.go
+++ client.go
@@ -644,10 +644,10 @@ func (r *Request) SetBody(body interface{}) *Request {
 }
 func (r *Request) Result(body interface{}) *Request {
 	switch body.(type) {
-	case *string:
-		r.responseBody = body
-	case *[]byte:
-		r.responseBody = body
+	// case *string:
+	// 	r.responseBody = body
+	// case *[]byte:
+	// 	r.responseBody = body
 	case io.Writer:
 		r.responseBody = body
 	default:
@@ -831,12 +831,21 @@ func (r *Request) invoke(ctx context.Context, method string) HTTPError {
 
 	// Install closing the request body (if any)
 	bodyCloser := resp.Body
+
 	defer func() {
 		if bodyCloser != nil {
 			io.Copy(ioutil.Discard, bodyCloser)
 			bodyCloser.Close()
 		}
 	}()
+
+	// if true {
+	// 	if bodyCloser != nil {
+	// 		out := io.TeeReader(bodyCloser, os.Stderr)
+	// 		bodyCloser = io.NopCloser(out)
+	//    resp.Body = bodyCloser
+	// 	}
+	// }
 
 	if !isOK {
 		if r.errorFunc != nil {
